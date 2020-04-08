d03db4c2d9ed533b7abc1d735bbfff530a058fdb
lyes <lyes.makhloufi@outlook.fr>
2020-04-08 22:18:05 +0200

    last changes

--- src/main/java/com/lyes/homeShop/web/BillServlet.java
+++ src/main/java/com/lyes/homeShop/web/BillServlet.java
@@ -1,14 +1,70 @@
 package com.lyes.homeShop.web;
 
+import com.lyes.homeShop.Fridge;
+import com.lyes.homeShop.Product;
+import com.lyes.homeShop.Television;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HelloWorldServlet extends HttpServlet {
+public class BillServlet extends HttpServlet {
+    List<Product> products = new ArrayList<Product>();
+    @Override
+    public void init() throws ServletException {
+
+        super.init();
+        Product cafe = new Product("Philips HD7866/61", "Philips SENSEO Quadrante, Noir - 1 ou 2 tasses", 79.99);
+        Product tv = new Television( 49, "LED","TV Samsung UE49MU6292", "Smart TV LED incurvée 49\"", 599);
+        Fridge fridge = new Fridge( 130, false,"BEKO TSE 1042 F", "Réfrigérateur BEKO 130L - Classe A+ - blanc", 189);
+        products.add(cafe);
+        products.add(tv);
+        products.add(fridge);
+    }
+
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        if (req.getQueryString() == null)
+            displayFrom(resp);
+        else {
+            displayBill(req,resp);
+        }
+    }
+
+    private void displayFrom(HttpServletResponse resp) throws IOException {
+        for (int i = 0; i < products.size(); i++) {
+            Product product = products.get(i);
+            resp.getWriter().println(
+                    "<b>" + i + " - " + product.getName() + "</b> : " + product.getPrice() + "<br/>" +
+                            product.getDescription() + "<br/><br/>");
+        }
+        String form = "<form action=\"bill\">" +
+                "<b>nom complet :</b> <input name=\"fullname\"/><br/>" +
+                "<b>adresse :</b> <input name=\"address\"/><br/><br/>" +
+                "<b>livraison :</b> <br/>" +
+                "à domicile : <input type=\"radio\" name=\"deliveryMode\" value=\"direct\"/><br/>" +
+                "express : <input type=\"radio\" name=\"deliveryMode\" value=\"express\"/><br/>" +
+                "point relais : <input type=\"radio\" name=\"deliveryMode\" value=\"relay\"/><br/>" +
+                "à retirer : <input type=\"radio\" name=\"deliveryMode\" value=\"takeAway\"/><br/>" +
+                "<b>Informations livraison</b> (relay et express) : <input name=\"deliveryInfo\"/><br/><br/>" +
+                "<b>liste produits </b> (produit:quantité, un produit par ligne) : <br/>" +
+                "<textarea name=\"products\"></textarea><br/>" +
+                "<input type=\"submit\"/>" +
+                "</form>";
+        resp.getWriter().println(form);
+    }
+
+    public void displayBill(HttpServletRequest req, HttpServletResponse resp) throws  IOException{
+
+        String name = req.getParameter("fullname");
+        resp.getWriter().println(name);
+
     }
 }
